92cacab30c0320b3ab92eb9c227dd7900865b81a
Jon Lindell <jhlindell@yahoo.com>
2017-05-24 14:22:45 -0600

    add password encryption

--- solution.js
+++ solution.js
@@ -1,9 +1,10 @@
+const bcrypt = require('bcrypt');
 function hash(plaintext) {
-
+  return bcrypt.hashSync(plaintext, 8);
 }
 
 function compare(plaintext, hash) {
-
+  return bcrypt.compareSync(plaintext, hash);
 }
 
 module.exports = {
